5973333cfb4492db079189b2c534b88c05bf51bb
BethMwangi <wanjikumwangib@gmail.com>
2021-05-03 22:45:01 +0300

    Quotes and commenting in php and how to escape characters

--- index.php
+++ index.php
@@ -11,3 +11,21 @@
 */
     print 'Hello World'; 
 ?>
+
+<?php
+echo "Joe's Nickname in HS was \"Joey Calzone\"";
+
+// Single quotes
+
+// Double quote
+// Any string in php will be processed by PHP before outputted. =>literal value
+?>
+<?php
+$name = 'Beth';
+echo "Hello, $name";
+?>
+
+
+<?php
+echo 'How do I crash Joe\'s course';
+// Escape characters ?>
